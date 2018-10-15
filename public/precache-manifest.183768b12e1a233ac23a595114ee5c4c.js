10bc42bf8e81244f3d147c003ca14f34e5b4635a
Ilham Moutaharik <ilham@prescrypto.com>
2018-10-15 13:35:41 -0500

    demo

---
+++ public/precache-manifest.183768b12e1a233ac23a595114ee5c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b1a7d19c7b3030292f4",
+    "url": "/static/js/main.3b1a7d19.chunk.js"
+  },
+  {
+    "revision": "7bc483103e029d5ca303",
+    "url": "/static/js/1.7bc48310.chunk.js"
+  },
+  {
+    "revision": "3b1a7d19c7b3030292f4",
+    "url": "/static/css/main.0490d328.chunk.css"
+  },
+  {
+    "revision": "7bc483103e029d5ca303",
+    "url": "/static/css/1.01b10b1a.chunk.css"
+  },
+  {
+    "revision": "bb69b91ab8be4082e60c42a1f9c97bde",
+    "url": "/index.html"
+  }
+];
